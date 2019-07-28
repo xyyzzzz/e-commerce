037e6839b884bddcc396b8ae2f68fc1e74a4a742
CHNan <987266515@qq.com>
2019-07-28 16:26:50 +0800

    index commit

---
+++ mservice-user/src/main/java/com/ecommerce/mservicce/user/index/IndexController.java
@@ -0,0 +1,22 @@
+package com.ecommerce.mservicce.user.index;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+
+    @RequestMapping("/index")
+    public Object greeting(@RequestParam(value="name", defaultValue="World") String name) {
+
+        Map map = new HashMap<>();
+        map.put("name",name);
+        map.put("content",String.format("Hi %s, lets begin.",name));
+        return map;
+    }
+}
